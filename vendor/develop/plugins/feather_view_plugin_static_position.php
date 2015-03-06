eddc78432111869d0316abe78e27b18cccead9cc
jsyczhanghao <jsyczhanghao@sina.com>
2015-03-06 11:34:30 +0800

    fixed bug

--- vendor/develop/plugins/feather_view_plugin_static_position.php
+++ vendor/develop/plugins/feather_view_plugin_static_position.php
@@ -1,6 +1,6 @@
 <?php
 class Feather_View_Plugin_Static_Position extends Feather_View_Plugin_Abstract{
-    public function exec($path, $content = '', Feather_View $view){
+    public function exec($path, $content = ''){
         if(!preg_match('#</head>#', $content)){
             $content = '
             <?php  
